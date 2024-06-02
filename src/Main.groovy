fb971a4674864355c5fd455d7388d8d2801928b9
Josh Matcuk <jmatcuk@gmail.com>
2024-06-02 15:52:22 -0500

    Trying to get all of the values into a list of structure for each row of the values in a CVS format.

--- src/Main.groovy
+++ src/Main.groovy
@@ -18,8 +18,45 @@ static void main(String[] args) {
 // Access and process data within the collection
   Globals.jsonCollection.each { map ->
     println "Data: ${map}" // You can access individual elements from the map here
+    weightValue = new WeightValue()
 
+    if(map.containsKey(WeightKeys.date)) {
+      weightValue.date = map[WeightKeys.date]
+    }
+    if(map.containsKey(WeightKeys.fat)) {
+      weightValue.fat = map[WeightKeys.fat]
+    }
+    if(map.containsKey(WeightKeys.weight)) {
+      weightValue.weight = map[WeightKeys.weight]
+    }
+    if(map.containsKey(WeightKeys.logId)) {
+      weightValue.logId = map[WeightKeys.logId]
+    }
+    if(map.containsKey(WeightKeys.time)) {
+      weightValue.time = map[WeightKeys.time]
+    }
+    if(map.containsKey(WeightKeys.source)) {
+      weightValue.source = map[WeightKeys.source]
+    }
+    if(map.containsKey(WeightKeys.bmi)) {
+      weightValue.bmi = map[WeightKeys.bmi]
+    }
+    //Unique Keys: [date, fat, weight, logId, time, source, bmi] across all of the json files.
+    weightValues.add(weightValue)
   }
+
+  def allKeys = new HashSet<String>()
+
+  Globals.jsonCollection.each { map ->
+    allKeys.addAll(map.keySet()) // Add all keys from each map to the set
+  }
+
+  def uniqueKeys = allKeys.toList().unique() // Convert set to list and get unique elements
+
+  println "Unique Keys: ${uniqueKeys}"
+
+  println WeightKeys.toCsv()
+
 }
 
 def processDirectory(File dir) {
@@ -29,7 +66,7 @@ def processDirectory(File dir) {
     } else if (file.name.endsWith(Globals.jsonExtension)) {
       // Parse JSON and convert to desired structure
       def jsonData = new JsonSlurper().parse(file)
-      Globals.jsonCollection << convertJsonToMap(jsonData) // Add parsed data to collection
+      convertJsonToMap(jsonData) // Add parsed data to collection
     }
   }
 }
@@ -48,7 +85,7 @@ def convertJsonToMap(data) {
   try {
     // Attempt to iterate through data as a map
     data.each { key, value -> map[key] = value }
-  } catch (MissingMethodException e) {
+  } catch (ignored) {
     // Handle cases where data isn't a map
     if (data instanceof List) {
       // If data is a list, iterate through its elements
@@ -66,4 +103,38 @@ def convertJsonToMap(data) {
 class Globals {
   static String jsonExtension = '.json'
   static ArrayList jsonCollection = []
+  static ArrayList<WeightValue> weightValues = []
+}
+
+
+//Unique Keys: [date, fat, weight, logId, time, source, bmi] across all of the json files.
+
+class WeightKeys {
+  static String logId = 'logId'
+  static String date = 'date'
+  static String time = 'time'
+  static String weight = 'weight'
+  static String bmi = 'bmi'
+  static String fat = 'fat'
+  static String source = 'source'
+
+    static String toCsv() {
+    // Build the CSV string with property values separated by commas
+    return "${logId}, ${date}, ${time}, ${weight}, ${bmi}, ${fat}, ${source}"
+  }
+}
+
+class WeightValue {
+  String logId = ""
+  String date = ""
+  String time = ""
+  String weight = ""
+  String bmi = ""
+  String fat = ""
+  String source = ""
+
+  static String toCsv() {
+    // Build the CSV string with property values separated by commas
+    return "${logId}, ${date}, ${time}, ${weight}, ${bmi}, ${fat}, ${source}"
+  }
 }
